a2ef781dc96e83ecce4c9918616261d580d280b7
gustavo <gustavo.zts@gmail.com>
2020-05-14 13:58:15 -0300

    login ok, faltando o botão de outras opções

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ifood_flutter/pages/home.dart';
-// import 'package:ifood_flutter/pages/home.dart';
-// teste 123
-// teste 123
 
 import 'pages/login.dart';
 
@@ -26,51 +23,6 @@ class MyApp extends StatelessWidget {
         '/restaurantes' : (BuildContext context) => new THome(),
       },
       home: TLogin(),
-      // home: MyHomePage(title: 'Ifood in Flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:ifood_flutter/components/button.dart';
-// import 'package:ifood_flutter/components/input.dart';
-// implementação de feature
+
 class TLogin extends StatefulWidget {
   @override
   _TLoginState createState() => _TLoginState();
@@ -11,102 +9,144 @@ class _TLoginState extends State<TLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(1),
-        child: AppBar(
-          backgroundColor: Theme.of(context).accentColor,
-          brightness: Brightness.light,
-        ),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Image.asset(
-                'assets/images/people-buying.png',
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(top: 10.0, left: 90.0, right: 90.0),
-              child: Text(
-                'Falta pouco pra matar sua fome!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Arial',
-                  // fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(top: 10.0, left: 90.0, right: 90.0),
-              child: Text(
-                'Como deseja continuar?',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Arial',
-                  color: Colors.black38,
-                  // fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
-              child: Container(
-                height: 60,
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              GestureDetector(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 25),
+                  child: Icon(
+                    Icons.keyboard_arrow_left,
+                    size: 40.0,
+                    color: Colors.red,
                   ),
-                  color: Colors.blueAccent,
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            color: Colors.white,
-                          ),
-                          child: Row(
-                            children: <Widget>[
-                              Image.asset(
-                                'assets/images/google-logo.png',
-                              ),
-                            ],
-                          ),
+                ),
+                onTap: () {
+                  Navigator.of(context).pushReplacementNamed('/home');
+                },
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+                child: Image.asset(
+                  'assets/images/people-buying.png',
+                  height: 280,
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, left: 90.0, right: 90.0),
+            child: Text(
+              'Falta pouco pra matar sua fome!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Arial',
+                // fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0, left: 5.0, right: 5.0),
+            child: Text(
+              'Como deseja continuar?',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Arial',
+                color: Colors.black38,
+                // fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+            child: Container(
+              height: 60,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                color: Colors.blueAccent,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white,
                         ),
-                      ),
-                      Expanded(
-                        flex: 6,
-                        child: Container(
-                          child: Text(
-                            'Continuar com o Google',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontFamily: 'Arial',
-                              color: Colors.white,
-                              fontSize: 16,
+                        child: Row(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/google-logo.png',
                             ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 6,
+                      child: Container(
+                        child: Text(
+                          'Continuar com o Google',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Arial',
+                            color: Colors.white,
+                            fontSize: 16,
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  onPressed: () {
-                    print('Continuar com o Google');
-                  },
+                    ),
+                  ],
                 ),
+                onPressed: () {
+                  print('Continuar com o Google');
+                },
               ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+            child: Container(
+              height: 60,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  side: BorderSide(color: Colors.black38),
+                ),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 6,
+                      child: Container(
+                        child: Text(
+                          'Outras opções',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Arial',
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                onPressed: () {
+                  print('Outras Opções');
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
